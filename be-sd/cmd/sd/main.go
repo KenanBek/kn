ca8ea8ce67504e2c552d72bcab6bccd58c8835d9
Kanan Rahimov <mail@kenanbek.me>
2019-12-21 23:19:57 +0100

    [be-sd] setup service discovery for existing services

--- be-sd/cmd/sd/main.go
+++ be-sd/cmd/sd/main.go
@@ -1,17 +1,74 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"syscall"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Application: kn-be-sd URL Input: %s!", r.URL.Path[1:])
+const (
+	// ENForBESdPort is exported.
+	ENForBESdPort = "KN_BE_SD_PORT"
+	// ENForBESd is exported.
+	ENForBESd = "KN_BE_SD"
+	// ENForBEApi is exported.
+	ENForBEApi = "KN_BE_API"
+	// ENForFEWeb is exported.
+	ENForFEWeb = "KN_FE_WEB"
+)
+
+// SD struct used to keep information about internal services.
+type SD struct {
+	BESdPort string `json:"be_sd_port"`
+	BESd     string `json:"be_sd"`
+	BEApi    string `json:"be_api"`
+	FEWeb    string `json:"fe_web"`
+}
+
+func NewSD() *SD {
+	sdPort, _ := syscall.Getenv(ENForBESdPort)
+	sdURL, _ := syscall.Getenv(ENForBESd)
+	apiURL, _ := syscall.Getenv(ENForBEApi)
+	webURL, _ := syscall.Getenv(ENForFEWeb)
+
+	sd := SD{
+		BESdPort: sdPort,
+		BESd:     sdURL,
+		BEApi:    apiURL,
+		FEWeb:    webURL,
+	}
+
+	return &sd
+}
+
+var sd *SD
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+
+	// prepare json or return error response
+	js, err := json.Marshal(sd)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// set content type
+	w.Header().Set("Content-Type", "application/json")
+
+	// write final response or return error response
+	_, err = w.Write(js)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	sd = NewSD()
+
+	http.HandleFunc("/", handlerIndex)
+
 	log.Println("Application: kn-be-sd")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Port: %s\n", sd.BESdPort)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", sd.BESdPort), nil))
 }
