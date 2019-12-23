ae50b02d74ca0b330ea20af3fb31af0ddce8d517
Kanan Rahimov <mail@kenanbek.me>
2019-12-23 01:50:41 +0100

    Add code doc for Crawler.Crawl

--- be-se/internal/app/crawler/crawler.go
+++ be-se/internal/app/crawler/crawler.go
@@ -50,25 +50,32 @@ type WebCrawler struct {
 
 // Crawl is exported.
 func (wc *WebCrawler) Crawl() {
+	// Load the source links by the given source loader.
 	ss, err := wc.sl.Load()
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "source load error"))
 	}
 
+	// Iterate over the source links and check internal links.
+	// TODO: Check each iteration withing goroutine (#).
 	for _, s := range ss {
+		// Compile article regexp so it can be checked faster.
 		ar, err := regexp.Compile(s.ArticleRegexp)
 		if err != nil {
 			log.Fatalln(errors.Wrap(err, "error while compiling regexp"))
 		}
 
+		// Get all the links within the source link.
 		urls, err := wc.scraper.GetLinks(s.SourceURL)
 		if err != nil {
 			log.Println(err)
 		}
 
+		// Iterate over the found links and check for article links.
+		// All found links will be checked with database and if it is already
+		// processed it will be skipped.
 		for _, url := range urls {
 			h := domain.Hash(url)
-
 			hasLink := wc.repository.HasLink(h)
 
 			if !hasLink {
